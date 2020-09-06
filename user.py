698ca5bba7b0de654b0159a9f01bba4cd8462c7e
EmmanuelWanzala <wanzalaemmanuel28@gmail.com>
2020-09-06 16:13:00 +0300

    return user

--- user.py
+++ user.py
@@ -1,12 +1,12 @@
 from credentials import User, Credentials
 
 
-# def create_new_user(username,password):
-#     '''
-#     Function to create a new user with a username and password
-#     '''
-#     new_user = User(username,password)
-#     return new_user
+def create_new_user(username,password):
+    '''
+    Function to create a new user with a username and password
+    '''
+    new_user = User(username,password)
+    return new_user
 
 def save_user(user):
     '''
